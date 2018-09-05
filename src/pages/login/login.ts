b97221c2635947e1a105d146d7bea55b472833a4
BudaNyoni <nyonisbuda@gmail.com>
2018-09-05 15:41:43 +0200

    uyes

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
 import { RegisterPage } from '../register/register';
 
 import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
