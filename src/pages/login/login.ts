52f975892f0f30c7b19c7c75118b1a89ac04b0d9
Buhlebenkosi Tumelo Bosman <buhletumibosman@gmail.com>
2018-09-05 15:00:52 +0200

    login2

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RegisterPage } from '../register/register';
-
 import { IonicPage, NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
 import { FirebaseProvider} from '../../providers/firebase/firebase';
 import {login} from '../../Modals/login'
+import { HomePage } from '../home/home';
 
 
 /**
@@ -28,6 +26,7 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl:AlertController,private firebaseService:FirebaseProvider,public loadingCtrl:LoadingController) {
   }
+  
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
@@ -38,59 +37,59 @@ Reg(){
 }
 
 
-  showForgotPassword(){
-    const prompt = this.alertCtrl.create({
-      title: 'Enter Your Email',
-      message: "A new password will be sent to your email",
-      inputs: [
-        {
-          name: 'recoverEmail',
-          placeholder: 'you@example.com'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Submit',
-          handler: data => {
+showForgotPassword(){
+  const prompt = this.alertCtrl.create({
+    title: 'Enter Your Email',
+    message: "A new password will be sent to your email",
+    inputs: [
+      {
+        name: 'recoverEmail',
+        placeholder: 'you@example.com'
+      },
+    ],
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Submit',
+        handler: data => {
 
-            const loader = this.loadingCtrl.create({
-              content: "Please wait.. resetting your password",
-              duration: 2000
+          const loader = this.loadingCtrl.create({
+            content: "Please wait.. resetting your password",
+            duration: 2000
+          });
+          loader.present();
+
+          this.firebaseService.forgotUserPassword(data.recoverEmail).then(() =>{
+            // add toast
+            loader.dismiss().then(() => {
+            //show pop up
+            let alert = this.alertCtrl.create({
+            title: 'Check your email',
+            subTitle: 'Password reset succesful',
+            buttons: ['OK']
             });
-            loader.present();
-
-            this.firebaseService.forgotUserPassword(data.recoverEmail).then(() =>{
-              // add toast
-              loader.dismiss().then(() => {
-                //show pop up
-                let alert = this.alertCtrl.create({
-                  title: 'Check your email',
-                  subTitle: 'Password reset succesful',
-                  buttons: ['OK']
-                });
-                alert.present();
-              })
-            }, error =>{ 
-              loader.dismiss().then(() => {
-              let alert = this.alertCtrl.create({
-                title: 'Error resseting password',
-                subTitle:error.message,
-                buttons: ['OK']
-              });
               alert.present();
             })
+          },error =>{ 
+            loader.dismiss().then(() => {
+            let alert = this.alertCtrl.create({
+            title: 'Error resseting password',
+            subTitle:error.message,
+            buttons: ['OK']
             });
-          }
+            alert.present();
+          })
+          });
         }
-      ]
-    });
-    prompt.present();
+      }
+    ]
+  });
+  prompt.present();
   }
 
   login(){
@@ -109,7 +108,9 @@ Reg(){
       });
       alert.present();
     })
+    .then(() =>{
+      this.navCtrl.push(HomePage);
+    })
   }
-
 
 }
