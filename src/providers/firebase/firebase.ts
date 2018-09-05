c7ca3faff4b52cb6a11801cbc6b47c984af9c977
Derik <deriksadiki@gmail.com>
2018-09-05 10:23:32 +0200

    provide

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+declare var firebase;
 /*
   Generated class for the FirebaseProvider provider.
 
@@ -9,7 +9,34 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FirebaseProvider {
 
+  database = firebase.database();
+  authnticate  = firebase.auth();
+
   constructor() {
+
+  }
+
+  login(email, password){
+
+  }
+
+  registerUser(email,password){
+
+  }
+  registerTalentPerson(email,password, name, surname, gender, cellno, age){
+
+  }
+
+
+  registerScoutPerson(email, password, name, surname, companyName, companyemail, companycellno, cellno, ){
+
+  }
+
+  logout(){
+
+  }
+
+  getUserSatate(){
 
   }
 
