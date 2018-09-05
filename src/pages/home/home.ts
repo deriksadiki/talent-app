69dfaf08b7a7731ef95ecfecc5483b4b114cd867
BudaNyoni <nyonisbuda@gmail.com>
2018-09-05 10:26:53 +0200

    uyes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SecondPage } from '../second/second';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  splash = true;
+  secomndPage = SecondPage;
   constructor(public navCtrl: NavController) {
 
   }
-
+  ionViewDidLoad(){
+    setTimeout(()=> this.splash = false , 3000);
+  }
 }
