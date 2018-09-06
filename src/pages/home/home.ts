4e74234312d45b538cb6f051d2ce20f87441d7ce
Buhlebenkosi Tumelo Bosman <buhletumibosman@gmail.com>
2018-09-06 12:53:02 +0200

    w

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,27 +13,6 @@ export class HomePage {
   constructor(public navCtrl: NavController,private firebaseService:FirebaseProvider,public alertCtrl:AlertController) {
   }
     // ionviewDidLoad(){
-  //   // setTimeout(()=> this.splash = false , 3000);
-  // }
-  
-  logout(){
-      this.firebaseService.authnticate.signOut().then(()=>{
-        const alert = this.alertCtrl.create({
-          title: 'Log Out',
-          subTitle: 'You have now been logged out!',
-          buttons: ['OK']
-        });
-        alert.present();
-        this.navCtrl.push(LoginPage);
-      }, Error =>{
-        const alert = this.alertCtrl.create({
-          title: 'Warning',
-          subTitle: Error,
-          buttons: ['OK']
-        });
-        alert.present();
-      })
-    }
-
-  }
- 
+    // setTimeout(()=> this.splash = false , 3000);
+    // }
+}
