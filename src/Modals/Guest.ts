d1a5cfcb80a49b4fe6224ca28908923c9fdbc4bf
Buhlebenkosi Tumelo Bosman <buhletumibosman@gmail.com>
2018-09-06 08:28:33 +0200

    w

--- src/Modals/Guest.ts
+++
@@ -1,4 +0,0 @@
-export interface Guest{
-    email:string;
-    password:number;
-}
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the RecruiterPage page.
+ * Generated class for the ProfilePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-recruiter',
-  templateUrl: 'recruiter.html',
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
 })
-export class RecruiterPage {
+export class ProfilePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RecruiterPage');
+    console.log('ionViewDidLoad ProfilePage');
   }
 
 }
