b38e92c7c51892af742f7fee70da9d568a3a5fb6
Buhlebenkosi Tumelo Bosman <buhletumibosman@gmail.com>
2018-09-06 10:35:29 +0200

    LogoutDone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
 import { ProfilePage } from '../pages/profile/profile';
+import { LogoutPage } from '../pages/logout/logout';
 // import { LogoutPage } from '../pages/logout/logout';
 
 @Component({
@@ -14,9 +15,6 @@ import { ProfilePage } from '../pages/profile/profile';
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
-
-  rootPage: any = HomePage;
 
   public rootPage:any;
 
@@ -29,10 +27,10 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Profile', component: HomePage },
-      { title: 'Log-Out', component: LoginPage },
+      { title: 'Log-Out', component: LogoutPage },
       { title: 'List', component: ListPage }
     ];
-
+if('Log-Out')
     this. firebaseService.getUserSatate().then( data =>{
       if (data == 1){
        this.rootPage =  HomePage ;
---
+++ src/pages/user/user.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { FirebaseProvider} from '../../providers/firebase/firebase';
+import {Guest} from '../../Modals/Guest'
+/**
+* Generated class for the UserPage page.
+*
+* See https://ionicframework.com/docs/components/#navigation for more info on
+* Ionic pages and navigation.
+*/
+
+@IonicPage()
+@Component({
+ selector: 'page-user',
+ templateUrl: 'user.html',
+})
+export class UserPage {
+guest = {} as Guest;
+ constructor(public navCtrl: NavController, public navParams: NavParams,private firebaseService:FirebaseProvider,public alertCtrl:AlertController) {
+ }
+
+
+
+ reg(){
+   this.firebaseService.registerUser(this.guest.email, this.guest.password, this.guest.Username).then(() =>{
+     const alert = this.alertCtrl.create({
+       title: 'Welcome',
+       subTitle: 'You have successfully Registared',
+       buttons: ['OK']
+     });
+     this.navCtrl.push(HomePage);
+     alert.present();
+   }, Error =>{
+       const alert = this.alertCtrl.create({
+         title: 'Warning!',
+         subTitle: Error,
+         buttons: ['OK']
+       });
+       alert.present();
+     })
+ }
+
+}
