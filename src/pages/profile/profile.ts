0c7469585ea296951515c728706108e4dc2dcd5b
unknown <you@example.com>
2018-09-14 08:55:45 +0200

    yes

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -16,24 +16,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  arr = new Array();
-  dbRef;
-  datas;
+  
 
-  recruit = {} as Recruit;
-
-  email:string;
-  password:string;
-  name:string;
-  surname:string;
-  companyName:string;
-  companyemail:string;
-  gender:string;
-  age:string;
-  cellno:string;
-
-  constructor(public navCtrl: NavController, public navParams: NavParams,public firebaseService:FirebaseProvider) {
-    this.firebaseService.getuserType();
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
   }
 
   ionViewDidLoad() {
