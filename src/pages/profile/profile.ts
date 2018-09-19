5893f8629694986238e78de05547d27af0c2939d
unknown <you@example.com>
2018-09-19 09:37:27 +0200

    thulz

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ArtistProfileUpdatePage } from '../artist-profile-update/artist-profile-update';
 
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-profile',
@@ -16,14 +9,7 @@ import { ArtistProfileUpdatePage } from '../artist-profile-update/artist-profile
 })
 export class ProfilePage {
 
-<<<<<<< HEAD
-  
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    
-=======
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
->>>>>>> f93048f008b71425f8f8cef75762ef85946085e7
   }
 
   ionViewDidLoad() {
--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -212,8 +212,8 @@ storeToDB(name, category, vidname, vidDesc){
             username : this.username,
             userImg : this.imgurl,
             date : today,
-            likes : 0,
-            comments : 0
+            likes : 1,
+            comments : 1
           });
           accpt('success');
 }, Error =>{
@@ -241,7 +241,7 @@ getAllvideos(){
                 var key = keys2[a];
                 let obj = {
                 likes: details[key].likes,
-                comments : details[key].comments,
+                comments : details[key].comments - 1,
                 vidurl : details[key].downloadurl,
                 vidDesc : details[key].description,
                 vidname : details[key].name,
